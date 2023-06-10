fd31026bd3353d21c3119a6cffce9654ef9b6b75
UserNanno <juan.canecillas@unmsm.edu.pe>
2023-06-10 01:09:52 -0500

    Estructura base del proyecto

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+
+// settings -> Configuraciones
+
+
+
+
+// Middlewares -> Iran todas las funciones que serán ejecutadas antes de llegar al servidor 
+//                o cuando llegan al servidor, antes de pasarsela a las rutas
+
+
+
+// Global variables -> Colocar datos que toda la aplicación tenga acceso
+
+
+
+// Routes -> Para las rutas
+
+
+
+// Static Files -> Configurar donde estará la carpeta de los archivos estáticos
+
+
+
+// Inicializar servidor 
