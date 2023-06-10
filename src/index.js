99a18c3403ccbdb175dd05b4b055f22b4e06706e
UserNanno <juan.canecillas@unmsm.edu.pe>
2023-06-10 02:24:23 -0500

    Configuraciones basicas (Sesiones, views, servidor) y creación de archivos para las rutas

--- src/index.js
+++ src/index.js
@@ -1,15 +1,36 @@
 const express = require('express');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const methodOverride = require('method-override')
+const session = require('express-session')
+
+// Inicializations
+
 const app = express();
 
 // settings -> Configuraciones
 
-
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'))
+app.engine('.hbs', exphbs({
+    defaultLayout: 'main',
+    layaoutDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
 
 
 // Middlewares -> Iran todas las funciones que serán ejecutadas antes de llegar al servidor 
 //                o cuando llegan al servidor, antes de pasarsela a las rutas
 
-
+app.use(express.urlencoded({extended: false}));
+app.use(methodOverride('_method')); 
+app.use(session({
+    secret: 'mysecretapp',
+    resave: true,
+    saveUninitialized: true
+}));
 
 // Global variables -> Colocar datos que toda la aplicación tenga acceso
 
@@ -24,3 +45,7 @@ const app = express();
 
 
 // Inicializar servidor 
+
+app.listen(app.get('port'), () =>{
+    console.log("Server on port ", app.get('port'))
+});
